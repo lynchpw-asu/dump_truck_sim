383379380db8aac7bd367bca0d56f512744afdb1
lowels104 <70414165+lowels104@users.noreply.github.com>
2021-04-08 14:11:54 -0600

    Update index.js

--- front_end/index.js
+++ front_end/index.js
@@ -1,62 +1,130 @@
 const BG_COLOUR = '#231f20';
-const TRUCK_COLOUR = '#c2c2c2'
-const TRASH_COLOUR = '#e66916';
+const SNAKE_COLOUR = '#c2c2c2';
+const FOOD_COLOUR = '#e66916';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://sleepy-island-33889.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
+socket.on('gameOver', handleGameOver);
+socket.on('gameCode', handleGameCode);
+socket.on('unknownCode', handleUnknownCode);
+socket.on('tooManyPlayers', handleTooManyPlayers);
 
 const gameScreen = document.getElementById('gameScreen');
+const initialScreen = document.getElementById('initialScreen');
+const newGameBtn = document.getElementById('newGameButton');
+const joinGameBtn = document.getElementById('joinGameButton');
+const gameCodeInput = document.getElementById('gameCodeInput');
+const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+
+newGameBtn.addEventListener('click', newGame);
+joinGameBtn.addEventListener('click', joinGame);
+
+
+function newGame() {
+  socket.emit('newGame');
+  init();
+}
+
+function joinGame() {
+  const code = gameCodeInput.value;
+  socket.emit('joinGame', code);
+  init();
+}
 
 let canvas, ctx;
+let playerNumber;
+let gameActive = false;
 
 function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+  initialScreen.style.display = "none";
+  gameScreen.style.display = "block";
 
-    canvas.width = canvas.height = 600;
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = BG_COLOUR;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  canvas.width = canvas.height = 600;
 
-    document.addEventListener('keydown', keydown);
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  document.addEventListener('keydown', keydown);
+  gameActive = true;
 }
 
 function keydown(e) {
-    console.log(e.keyCode);
+  socket.emit('keydown', e.keyCode);
 }
 
-init();
-
 function paintGame(state) {
-    ctx.fillStyle = BG_COLOUR;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const trash = state.trash;
-    const gridsize = state.gridsize;
-    const size = canvas.width / gridsize;
+  const food = state.food;
+  const gridsize = state.gridsize;
+  const size = canvas.width / gridsize;
 
-    ctx.fillStyle = TRASH_COLOUR;
-    ctx.fillRect(trash.x * size, trash.y * size, size, size);
+  ctx.fillStyle = FOOD_COLOUR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
 
-    paintPlayer(state.player, size, TRUCK_COLOUR);
+  paintPlayer(state.players[0], size, SNAKE_COLOUR);
+  paintPlayer(state.players[1], size, 'red');
 }
 
 function paintPlayer(playerState, size, colour) {
-    const truck = playerState.truck;
+  const snake = playerState.snake;
 
-    ctx.fillStyle = TRUCK_COLOUR;
-    for (let cell of truck) {
-        ctx.fillRect(cell.x * size, cell.y * size, size, size);
-    }
+  ctx.fillStyle = colour;
+  for (let cell of snake) {
+    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+  }
 }
 
-function handleInit(msg) {
-    console.log(msg);
+function handleInit(number) {
+  playerNumber = number;
 }
 
 function handleGameState(gameState) {
-    gameState = JSON.parse(gameState);
-    requestAnimationFrame(() => paintGame(gameState));
+  if (!gameActive) {
+    return;
+  }
+  gameState = JSON.parse(gameState);
+  requestAnimationFrame(() => paintGame(gameState));
+}
+
+function handleGameOver(data) {
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+
+  gameActive = false;
+
+  if (data.winner === playerNumber) {
+    alert('You Win!');
+  } else {
+    alert('You Lose :(');
+  }
+}
+
+function handleGameCode(gameCode) {
+  gameCodeDisplay.innerText = gameCode;
+}
+
+function handleUnknownCode() {
+  reset();
+  alert('Unknown Game Code')
+}
+
+function handleTooManyPlayers() {
+  reset();
+  alert('This game is already in progress');
+}
+
+function reset() {
+  playerNumber = null;
+  gameCodeInput.value = '';
+  initialScreen.style.display = "block";
+  gameScreen.style.display = "none";
 }
